e4cf3f54cd4ab34c5546217643adf79c27260bbf
unknown <c.lalanne92@gmail.com>
2020-02-21 12:51:16 -0300

    add additional information

--- zipkin/exe zipkin-server.bat
+++ zipkin/exe zipkin-server.bat
@@ -1,1 +1,8 @@
+@echo off
+set RABBIT_ADDRESSES=localhost:5672
+set STORAGE_TYPE=mysql
+set MYSQL_DB=zipkin_dev
+set MYSQL_USER=zipkin
+set MYSQL_PASS=zipkin
+set MYSQL_HOST= localhost:3306
 java -jar zipkin-server-2.19.3-exec.jar
